89a9c81c2918907df18790d093164b8924a75931
Sulaiman <sulaimansiddiqui55@gmail.com>
2025-10-11 13:46:55 -0500

    Update camera.java

--- camera.java
+++ camera.java
@@ -1,67 +1,84 @@
-import org.opencv.core.*;
-import org.opencv.videoio.VideoCapture;
-import org.opencv.imgproc.Imgproc;
-import april.tag.AprilTagDetector;
-import april.tag.TagDetection;
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+import org.firstinspires.ftc.vision.VisionPortal;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 import java.util.List;
 
-public class AprilTagCamera {
-    static { System.loadLibrary(Core.NATIVE_LIBRARY_NAME); }
-
-    public static void main(String[] args) {
-
-        VideoCapture camera = new VideoCapture(0);
-        if (!camera.isOpened()) {
-            System.out.println("Error: Camera not accessible");
-            return;
-        }
 
 
-        AprilTagDetector detector = new AprilTagDetector();
+public class BaseSubsystem{
+    DcMotor fl, fr, br, bl;
+    private AprilTagProcessor aprilTag;
 
-        Mat frame = new Mat();
-        while (true) {
-            camera.read(frame);
-            if (frame.empty()) continue;
+    private VisionPortal visionPortal;
+    
+    BaseSubsystem(HardwareMap hardwareMap){
+        fl = hardwareMap.get(DcMotor.class, "name");
 
+        aprilTag = new AprilTagProcessor.Builder().build();
 
-            Mat gray = new Mat();
-            Imgproc.cvtColor(frame, gray, Imgproc.COLOR_BGR2GRAY);
+        VisionPortal.Builder builder = new VisionPortal.Builder();
+        
+        builder.setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"));
+        builder.addProcessor(aprilTag);
 
-
-            List<TagDetection> detections = detector.detect(gray);
-
-            for (TagDetection detection : detections) {
-                int tagId = detection.id;
-                Point center = new Point(detection.cx, detection.cy);
-
-                String label;
-                Scalar color;
-
-                if (tagId == 1) {
-                    label = "Tag 1 detected - Action A";
-                    color = new Scalar(0, 255, 0); // Green
-                } else if (tagId == 2) {
-                    label = "Tag 2 detected - Action B";
-                    color = new Scalar(0, 0, 255); // Red
-                } else {
-                    label = "Unknown Tag " + tagId + " - Default action";
-                    color = new Scalar(255, 0, 0); // Blue
-                }
-
-
-                Imgproc.circle(frame, center, 10, color, -1);
-                Imgproc.putText(frame, label, new Point(center.x + 10, center.y),
-                        Imgproc.FONT_HERSHEY_SIMPLEX, 0.5, color, 2);
-            }
-
-
-
-
-         if ()
-        }
-
-        camera.release();
+        visionPortal = builder.build();
     }
+    
+    public void stopMotors(){
+        fl.setPower(0.0);
+        fr.setPower(0.0);
+        bl.setPower(0.0);
+        br.setPower(0.0);
+    }
+    
+    
+    void stopAprilTagStream() {
+        visionPortal.stopStreaming();
+    }
+    
+     void resumeAprilTagStream() {
+        visionPortal.resumeStreaming();
+    }
+    
+
+      
+
+    
+    void telemetryAprilTag(Telemetry telemetry) {
+
+        List<AprilTagDetection> currentDetections = aprilTag.getDetections();
+        telemetry.addData("# AprilTags Detected", currentDetections.size());
+
+        
+        for (AprilTagDetection detection : currentDetections) {
+            if (detection.metadata != null) {
+                telemetry.addLine(String.format("\n==== (ID %d) %s", detection.id, detection.metadata.name));
+                telemetry.addLine(String.format("XYZ %6.1f %6.1f %6.1f  (inch)", detection.ftcPose.x, detection.ftcPose.y, detection.ftcPose.z));
+                telemetry.addLine(String.format("PRY %6.1f %6.1f %6.1f  (deg)", detection.ftcPose.pitch, detection.ftcPose.roll, detection.ftcPose.yaw));
+                telemetry.addLine(String.format("RBE %6.1f %6.1f %6.1f  (inch, deg, deg)", detection.ftcPose.range, detection.ftcPose.bearing, detection.ftcPose.elevation));
+            } else {
+                telemetry.addLine(String.format("\n==== (ID %d) Unknown", detection.id));
+                telemetry.addLine(String.format("Center %6.0f %6.0f   (pixels)", detection.center.x, detection.center.y));
+            }
+        }   
+        telemetry.addLine("\nkey:\nXYZ = X (Right), Y (Forward), Z (Up) dist.");
+        telemetry.addLine("PRY = Pitch, Roll & Yaw (XYZ Rotation)");
+        telemetry.addLine("RBE = Range, Bearing & Elevation");
+
+    }  
+
 }
+
+
+
+
+
